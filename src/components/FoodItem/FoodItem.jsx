2db95e49a6106ef9155baa1815216fa09c77dfad
Mukesh <dasriyamukesh87@gmail.com>
2024-05-23 09:10:19 +0530

    checkout button added

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -147,7 +147,7 @@ useEffect(()=> {
    {visi &&  <div className="checkout_button absolute left-[90%] ">
     {/* <button className="fixed border-2 border-black bg-black text-white p-1 rounded-md">Checkout</button> */}
     <button onClick={()=>navigate("/cart")} type="button" className="checkout_button  fixed z-10 text-white bg-black hover:bg-black focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-CHECKOU
+CHECKOUT
 <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
 </svg>
