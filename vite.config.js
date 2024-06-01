b3ce8847d179fee3e424202a450bebc09079c6e9
Mukesh <dasriyamukesh87@gmail.com>
2024-06-01 10:08:40 +0530

    Free devlivery added

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { terser } from "rollup-plugin-terser";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [
-    react(),
-    terser({
-      compress: {
-        drop_console: true, // Removes console logs
-        drop_debugger: true, // Removes debugger statements
-      },
-      mangle: {
-        properties: {
-          regex: /^_/ // Mangles properties that start with an underscore
-        }
-      }
-    })
-  ],
-  build: {
-    sourcemap: false, // Ensures source maps are not generated
-    rollupOptions: {
-      output: {
-        manualChunks: undefined, // Disable code splitting for simplicity
-      }
-    }
-  }
+  plugins: [react()],
 });
