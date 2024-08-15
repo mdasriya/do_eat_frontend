56c0b5a0866351a62c0eefa77fdf7a9f48c9389e
Mukesh <dasriyamukesh87@gmail.com>
2024-08-15 08:36:50 +0530

    app

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import './AppDownload.css'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
 
 const AppDownload = () => {
   return (
     <div className='app-download' id='app-download'>
       <p>For better experience, download <br /> the Do Eat app now</p>
       <div className="app-download-platform">
-        <img src={assets.play_store} alt="" />
+      <Link to='https://play.google.com/store/apps/details?id=com.codedeals.doeats'>
+      <img src={assets.play_store} alt="" />  </Link>
         <img src={assets.app_store} alt="" />
+       
       </div>
     </div>
   )
